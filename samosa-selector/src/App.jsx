c767a8ccf47caa8e1692665f44fd2e576596ae9a
Angie <Angie.Rivera81@bcmail.cuny.edu>
2025-09-24 21:44:28 -0400

    Update App.jsx
    added the count and multipler and app container and imported ussestate

--- samosa-selector/src/App.jsx
+++ samosa-selector/src/App.jsx
@@ -1,20 +1,29 @@
+import { useState } from 'react';
 import './App.css'
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
 
+  const updateCount = () => setCount(count + multiplier);
   return (
     <div className="App">
-      <div className= "header">
+      <div className="app-container">
+      <div className="header">
         <h1>Samosa Selector</h1>
-        <h2>Count: </h2>
+
+        <h2>Count: {count}</h2>
+
         <img
-          className="samosa" 
-          src="../src/assets/images/Samosa.jpg"
-          alt="Samosa" 
-        />
+          className="samosa"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1aAsZ_HEnDRiInJJQehosA7P9-9idHS1kpQ&s"
+          alt="Samosa"
+          onClick={updateCount}/>
+        </div>
       </div>
     </div>
   );
 }
 
-export default App
+export default App;
+
