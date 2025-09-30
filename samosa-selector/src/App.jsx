969b38b609cfada7ba99e4f5220a42e465630689
Angie <Angie.Rivera81@bcmail.cuny.edu>
2025-09-30 15:12:25 -0400

    Summary

--- samosa-selector/src/App.jsx
+++ samosa-selector/src/App.jsx
@@ -11,19 +11,38 @@ const App = () => {
       <div className="app-container">
       <div className="header">
         <h1>Samosa Selector</h1>
-
         <h2>Count: {count}</h2>
 
         <img
           className="samosa"
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1aAsZ_HEnDRiInJJQehosA7P9-9idHS1kpQ&s"
           alt="Samosa"
-          onClick={updateCount}/>
+          onClick={updateCount}
+          />
+        </div>
+      </div>
+      <div className='container'>
+        <div className="upgrade">
+          <h3>Double Stuffed 👯‍♀️</h3>
+          <p>Each Click give you 2x samosas!</p>
+          <button>Cost: 10 samosas</button>
+        </div>
+
+        <div className="upgrade">
+          <h3>Party Pack 🎉</h3>
+          <p>Your Clicks are 5x stronger!</p>
+          <button>Cost: 100 samosas</button>
+        </div>
+
+        <div className="upgrade">
+          <h3>Full Feast👩🏽‍🍳 </h3>
+          <p>Your Clicks are 5x stronger!</p>
+          <button>Cost: 1000 samosas</button>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
 
